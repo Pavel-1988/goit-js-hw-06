f1c67443f5032ca7bdba62fe0b6bf2ba6e3c3af5
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-07-18 12:34:04 +0300

    9 из 10

--- js/task-09.js
+++ js/task-09.js
@@ -2,27 +2,24 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const sec = document.querySelector('body');
+const body = document.querySelector('body');
 const btn = document.querySelector('.change-color');
 const color = document.querySelector('.color');
 
 
 
 const onChangeBody = (event) => {
-  sec.style.backgroundColor = "#4caf50";
-  color.textContent = event.currentTarget.backgroundColor.value;
+  body.style.backgroundColor = getRandomHexColor ();
+  color.textContent = body.style.backgroundColor
 }
 
 
 
 btn.addEventListener("click", onChangeBody)
-btn.addEventListener("change", onChangeColor)
+
 
 
   
   
 
-//тут поменял цвет боди на конкретный
-// const changeBody = () => {
-//   sec.style.backgroundColor = "#4caf50"
-// }
+
